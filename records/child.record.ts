ca63ebcb5e6309e935b07a4dcd527d4da6bec66c
Radek Holubowski <radekh8787@gmail.com>
2022-02-13 17:52:28 +0000

    install @types/uuid, and add types to child.record file

--- records/child.record.ts
+++ records/child.record.ts
@@ -1,23 +1,24 @@
-const { pool } = require("../utils/db");
-const { ValidationError } = require("../utils/errors");
-const { v4: uuid } = require("uuid");
+import { pool } from "../utils/db";
+import { ValidationError } from "../utils/errors";
+import { v4 as uuid } from "uuid";
+import { FieldPacket } from "mysql2";
 
-class ChildRecord {
-  constructor(obj) {
+type ChildRecordResult = [ChildRecord[], FieldPacket[]];
+
+export class ChildRecord {
+  public id?: string;
+  public name: string;
+  public giftId: string;
+  constructor(obj: ChildRecord) {
     if (!obj.name || obj.name.length < 3 || obj.name.length > 25) {
       throw new ValidationError("Name must be at least 25 characters");
     }
-
-    this.id = obj.id;
-    this.name = obj.name;
-    this.giftId = obj.giftId;
   }
 
-  async insert() {
+  async insert(): Promise<string> {
     if (!this.id) {
       this.id = uuid();
     }
-
     await pool.execute(
       "INSERT INTO `children`(`id`, `name`) VALUES(:id, :name)",
       {
@@ -25,28 +26,27 @@ class ChildRecord {
         name: this.name,
       }
     );
-
     return this.id;
   }
 
-  static async listAll() {
-    const [results] = await pool.execute(
+  static async listAll(): Promise<ChildRecord[]> {
+    const [results] = (await pool.execute(
       "SELECT * FROM `children` ORDER BY `name` ASC"
-    );
+    )) as ChildRecordResult;
     return results.map((obj) => new ChildRecord(obj));
   }
 
-  static async getOne(id) {
-    const [results] = await pool.execute(
+  static async getOne(id: string): Promise<ChildRecord | null> {
+    const [results] = (await pool.execute(
       "SELECT * FROM `children` WHERE `id` = :id",
       {
         id,
       }
-    );
+    )) as ChildRecordResult;
     return results.length === 0 ? null : new ChildRecord(results[0]);
   }
 
-  async update() {
+  async update(): Promise<void> {
     await pool.execute(
       "UPDATE `children` SET `name` = :name, `giftId` = :giftId WHERE `id` = :id",
       {
@@ -57,7 +57,3 @@ class ChildRecord {
     );
   }
 }
-
-module.exports = {
-  ChildRecord,
-};
